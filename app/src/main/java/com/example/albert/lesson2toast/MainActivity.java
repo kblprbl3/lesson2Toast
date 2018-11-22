29412cc8ea7de29fb65fbf1da1e65d39924eba93
Albert <paren.iz.kgz@gmail.com>
2018-11-23 00:23:55 +0600

    New Activity

--- app/src/main/java/com/example/albert/lesson2toast/MainActivity.java
+++ app/src/main/java/com/example/albert/lesson2toast/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.albert.lesson2toast;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText pass;
-    private Button btn,btn2;
+    private Button btn,btn_alert;
+    private RatingBar rating;
+    private TextView text_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public void addListenOnButton (){
         pass = (EditText)findViewById(R.id.editText);
         btn = (Button)findViewById(R.id.button);
-        btn2 = (Button)findViewById(R.id.button2);
+        btn_alert = (Button)findViewById(R.id.alert);
         btn.setOnClickListener(
                 new View.OnClickListener() {
                     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -37,23 +43,52 @@ public class MainActivity extends AppCompatActivity {
                         //При нажатие кнопки, кнопка изменится на красный с надписью Done
                         // НЕ заработал на моем смартфоне
                         btn.setText("Done");
-                        btn.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                        //btn.setBackground(getColor(android.R.color.holo_red_dark));
                         Toast.makeText( MainActivity.this, pass.getText(), Toast.LENGTH_SHORT).show();
                     }
                 }
         );
 
+        rating = (RatingBar)findViewById(R.id.ratingBar);
+        text_show = (TextView)findViewById(R.id.textView);
 
-        btn2.setOnClickListener(
+        rating.setOnRatingBarChangeListener(
+                new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        text_show.setText("Значение " + String.valueOf(rating));
+                    }
+                }
+        );
+
+
+        btn_alert.setOnClickListener(
                 new View.OnClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                     @Override
                     public void onClick(View v) {
                         //При нажатие кнопки, кнопка изменится на красный с надписью Done
                         // НЕ заработал на моем смартфоне
-                        btn.setText("Done");
-                        btn.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
-                        Toast.makeText( MainActivity.this, "Мы все поменяли", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText( MainActivity.this, "Мы все поменяли", Toast.LENGTH_SHORT).show();
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                        a_builder.setMessage("Вы хотите выйти ?")
+                                .setCancelable(false)
+                                .setPositiveButton("Да" , new DialogInterface.OnClickListener()
+                                {
+                                    @Override
+                                    public void onClick (DialogInterface dialog, int which){
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick (DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = a_builder.create();
+                        alert.setTitle("Закрытие приложения");
+                        alert.show();
+
                     }
                 }
         );
